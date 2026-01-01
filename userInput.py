6b3ee950a766b9a469d2d2ab1b84a7fc71237c48
vinodh <yemireddivinodh@gmail.com>
2026-01-01 13:10:24 +0530

    this is genrators code in python

--- userInput.py
+++ userInput.py
@@ -1,3 +1,9 @@
-arr=list(map(int,input("enter the  arr elements to sort").split()))
+# Create a menu-driven program to perform basic list operations: add, delete, search, display.
+import sample
+import random
 
-print(arr)
+print("i am vinodh")
+# sample.say_hell0()
+# ch=random.choice([2,4,6,5,3,2])
+# ch=random.randrange(2,100)
+# print(ch)
